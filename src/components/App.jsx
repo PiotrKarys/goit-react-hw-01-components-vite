1410fda036efd050099d4d924be279dcf5ef4b0e
Piotrek <Piotr.karys96@gmail.com>
2024-06-22 18:46:53 +0200

    template commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,9 @@
-import React from "react";
-import Profile from "./profile/profile";
-import Statistics from "./stats/statistics";
-import FriendList from "./friendlist/friendlist";
-import TransactionHistory from "./transactions/transactionh";
-import transactions from "./transactions/transactions.json";
-import user from "./profile/user.json";
-import data from "./stats/data.json";
-import friends from "./friendlist/friends.json";
-const App = () => {
+function App() {
   return (
-    <div
-      style={{
-        // height: '100vh',
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: 40,
-        color: "#010101",
-        gap: 20,
-      }}>
-      React homework template
-      <Profile
-        avatar={user.avatar}
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </div>
+    <>
+      <h1>React Homework Template (Vite)</h1>
+    </>
   );
-};
+}
 
 export default App;
